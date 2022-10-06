887d9547df0bbafe3f08db27709fe771377b162e
kondziet <konrad.dabrowski54@gmail.com>
2022-10-06 14:08:53 +0200

    Add function which indicates winner

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const userChoice = document.querySelector("#user-input")
-const playButton = document.querySelector("button")
+const playButton = document.querySelector("#play-button")
 const gameStatus = document.querySelector("#game-status")
 
 const weapons = [
@@ -8,6 +8,26 @@ const weapons = [
     "scissors"
 ]
 
+const whatBeatsWhat = new Map()
+whatBeatsWhat.set("rock", "scissors")
+whatBeatsWhat.set("paper", "rock")
+whatBeatsWhat.set("scissors", "paper")
+
 function getComputerChoice() {
     return weapons[Math.floor(Math.random() * weapons.length)]
 }
+
+function playRound(userChoice, computerChoice) {
+    
+    if (userChoice == computerChoice) {
+        return "It's a draw!"
+    }
+
+    if (whatBeatsWhat.get(userChoice) == computerChoice) {
+        return "User wins game!"
+    } else {
+        return "Computer wins game!"
+    }
+}
+
+playButton.addEventListener("click", ()=>gameStatus.textContent = playRound(userChoice.value, getComputerChoice()))
