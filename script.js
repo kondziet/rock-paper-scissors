39027b48af5b7d099a461bb03db5275112bd6f3c
kondziet <konrad.dabrowski54@gmail.com>
2022-10-06 13:47:27 +0200

    Add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+const userChoice = document.querySelector("#user-input")
+const playButton = document.querySelector("button")
+const gameStatus = document.querySelector("#game-status")
+
+const weapons = [
+    "rock",
+    "paper",
+    "scissors"
+]
+
+function getComputerChoice() {
+    return weapons[Math.floor(Math.random() * weapons.length)]
+}
